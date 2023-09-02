c0ae258a6e8c23e0822aee07a5fda590e1c4045e
Param Patel <param.patel0767@gmail.com>
2023-09-02 01:04:26 -0500

    updated sanity config issue

--- sanity/sanity.cli.js
+++ sanity/sanity.cli.js
@@ -1,6 +1,9 @@
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
+  project: {
+    basePath: '/studio'
+  },
   api: {
     projectId: '9prc89n2',
     dataset: 'production'
