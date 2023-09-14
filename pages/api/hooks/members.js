69f2d7e8be4988aeb3fd7dd89f3940c1aa757154
Param Patel <param.patel0767@gmail.com>
2023-09-13 21:02:52 -0500

    team member section revalidation

---
+++ pages/api/hooks/members.js
@@ -0,0 +1,23 @@
+import { SIGNATURE_HEADER_NAME, isValidSignature } from "@sanity/webhook";
+
+const handler = async(req, res) => {
+    try {
+        const signature = req.headers[SIGNATURE_HEADER_NAME]
+        if (!isValidSignature(
+            JSON.stringify(req.body),
+            signature,
+            process.env.SANITY_WEBHOOK_SECRET
+        ))return res.status(401).json({msg:'Invalid Request!!'})
+
+
+        const {slug} = req.body
+        await res.revalidate(`/about`)
+        console.log("slug:", slug)
+        res.status(200).json({msg: 'Porduct pages revlaidated.'});
+
+    } catch (error) {
+        res.status(200).json({err:'Something went wrong!'})
+    }
+}
+
+export default handler;
